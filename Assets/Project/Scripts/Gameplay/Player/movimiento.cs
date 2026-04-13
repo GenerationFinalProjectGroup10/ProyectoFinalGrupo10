3cfa47d56f98467da5aa4c43c2de674b243ba30a
VortZh <johanreinan@gmail.com>
2026-04-13 10:40:18 -0500

    player funcionando con colisiones

--- Assets/Project/Scripts/Gameplay/Player/movimiento.cs
+++ Assets/Project/Scripts/Gameplay/Player/movimiento.cs
@@ -6,16 +6,24 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 input;
     private Rigidbody rb;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        // Input clásico
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
+
+        // Animaciones
+        animator.SetFloat("Horizontal", input.x);
+        animator.SetFloat("Vertical", input.y);
+        animator.SetBool("isMoving", input.sqrMagnitude > 0);
     }
 
     void FixedUpdate()
