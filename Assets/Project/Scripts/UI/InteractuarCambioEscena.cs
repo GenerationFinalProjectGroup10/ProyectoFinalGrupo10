6374d1272ccd781a74a5658bdfc3cdb962c6999c
RicardoPatino2667 <drpatinomurilloapps@gmail.com>
2026-04-23 10:36:13 -0500

    canvas interactuable

--- Assets/Project/Scripts/UI/InteractuarCambioEscena.cs
+++ Assets/Project/Scripts/UI/InteractuarCambioEscena.cs
@@ -1,10 +1,11 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class InteractuarCambioEscena : MonoBehaviour
 {
     public string nombreEscena;
     public KeyCode tecla = KeyCode.E;
+    public GameObject canvasInteraccion; // ← línea nueva
 
     private bool cerca = false;
 
@@ -19,12 +20,20 @@ public class InteractuarCambioEscena : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
             cerca = true;
+            if (canvasInteraccion != null)
+                canvasInteraccion.SetActive(true); // ← línea nueva
+        }
     }
 
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
             cerca = false;
+            if (canvasInteraccion != null)
+                canvasInteraccion.SetActive(false); // ← línea nueva
+        }
     }
 }
