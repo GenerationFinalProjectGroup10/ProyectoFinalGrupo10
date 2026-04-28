20cff910fb3bddb423b6a628ccca886a42f48bb4
Yamid-E <127436285+Yamid-E@users.noreply.github.com>
2026-04-27 21:35:26 -0500

    Condición de derrota _ Final Malo

--- Assets/Project/Scripts/UI/Temporizadorui.cs
+++ Assets/Project/Scripts/UI/Temporizadorui.cs
@@ -1,19 +1,38 @@
 ﻿using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement; 
 
 public class TemporizadorUI : MonoBehaviour
 {
     public TextMeshProUGUI texto;
+    private bool derrotaActivada = false;
 
     void Update()
     {
-        if (ClockManager.Instance == null) return;
+        if (ClockManager.Instance == null || derrotaActivada) return;
 
         var (h, m, s) = ClockManager.Instance.GetTimerTime();
 
-        Debug.Log("UI TIME: " + h + ":" + m + ":" + s);
-
         texto.text = $"{h:00}:{m:00}:{s:00}";
+
+        // CONDICIÓN DE DERROTA
+        if (h <= 0 && m <= 0 && s <= 0)
+        {
+            VerificarUbicacionJugador();
+        }
+    }
+
+    //Verificación escena
+    void VerificarUbicacionJugador()
+    {
+        string escenaActual = SceneManager.GetActiveScene().name;
+
+
+        if (escenaActual == "Mundo2" || escenaActual == "Mundo3")
+        {
+            derrotaActivada = true; 
+            SceneManager.LoadScene("Cinematica_Final_Oso");
+        }
     }
 
     public void IniciarTimer()
